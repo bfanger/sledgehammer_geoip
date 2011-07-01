97d8fecd794f832f333fc1b5788aa077d608a741
Bob Fanger <bfanger@gmail.com>
2011-07-02 01:32:26 +0200

    fixed utilhook

--- utils/getUtils.php
+++ utils/getUtils.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-		'update_db.php' => new UtilScript('update_db.php', 'Update GeoIP database'),
+	'update_db.php' => new SledgeHammer\UtilScript('update_db.php', 'Update GeoIP database'),
 );
 ?>
